d4e5864e391416c1208230bc42b7e7f9be326bf2
Manuel Bange <76457906+manubange@users.noreply.github.com>
2023-11-07 10:59:36 +0100

    Update determine_BetaBursts.m

--- determine_BetaBursts.m
+++ determine_BetaBursts.m
@@ -4,8 +4,8 @@ clc;  close all; warning off all; clearvars;
 % preparation
 fileScript = matlab.desktop.editor.getActiveFilename;
 [pathProject,name,ext] = fileparts(fileScript) ;                % path to working directory/current folder
-pathData = ([pathProject '/data'])
-pathFT = [pathProject '/fieldtrip-20220310']
+pathData = ([pathProject '/data']);
+pathFT = [pathProject '/fieldtrip-20220310'];
 
 addpath (pathProject)
 BetaThresh = 0.75;                                                              % burst definition at 75% percentile of amplitude according to Tinkhauser et al. 2020 Jneurosci
